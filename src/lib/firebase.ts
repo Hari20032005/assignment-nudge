9aa8862a223d6aeac229e8be50d4cfeb63678a93
Hariharan S <158813030+Hari20032005@users.noreply.github.com>
2025-03-05 19:44:32 +0530

    new

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,15 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Firebase configuration (these are safe to be in the client)
 const firebaseConfig = {
-  apiKey: "AIzaSyDyFOWGIe7KGeMjR5wJ9_9I2rNAkEgXUa0",
-  authDomain: "vit-assignment-reminder.firebaseapp.com",
-  projectId: "vit-assignment-reminder",
-  storageBucket: "vit-assignment-reminder.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:1234567890123456789012"
+  apiKey: "AIzaSyDKNh2PEggT0WShf-6jR4r8yHhdNnmYal4",
+  authDomain: "assignmentreminder-2b89c.firebaseapp.com",
+  projectId: "assignmentreminder-2b89c",
+  storageBucket: "assignmentreminder-2b89c.firebasestorage.app",
+  messagingSenderId: "335538143563",
+  appId: "1:335538143563:web:976d7b8f6a8ba00f3ea8ce"
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
